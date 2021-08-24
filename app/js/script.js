53ab6dc7e5f779287d788a658c273fc83f277d56
EN <iwagner76@gmail.com>
2021-08-23 21:29:06 -0400

    added footer styles and started js menu active

--- app/js/script.js
+++ app/js/script.js
@@ -21,3 +21,25 @@ const linkAction = () => {
 };
 
 navLink.forEach((link) => link.addEventListener("click", linkAction));
+
+const sections = document.querySelectorAll("section[id]");
+
+let scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+};
